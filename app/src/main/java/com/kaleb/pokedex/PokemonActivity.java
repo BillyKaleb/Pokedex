145455a1ee5a210ae94b4d0f0d63e97becfa9c73
Billy Kaleb <billykaleb@yahoo.com>
2018-11-11 03:57:21 +0900

    Initial commit

---
+++ app/src/main/java/com/kaleb/pokedex/PokemonActivity.java
@@ -0,0 +1,13 @@
+package com.kaleb.pokedex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PokemonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pokemon);
+    }
+}
