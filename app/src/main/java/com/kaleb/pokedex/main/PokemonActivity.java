e2990882cb015ac81fb0113dbfefc147cd9253f1
Billy Kaleb <billykaleb@yahoo.com>
2018-11-11 04:02:41 +0900

    Initial - Foldering. TODO: Boilerplate

--- app/src/main/java/com/kaleb/pokedex/main/PokemonActivity.java
+++ app/src/main/java/com/kaleb/pokedex/main/PokemonActivity.java
@@ -1,7 +1,9 @@
-package com.kaleb.pokedex;
+package com.kaleb.pokedex.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.kaleb.pokedex.R;
 
 public class PokemonActivity extends AppCompatActivity {
 
