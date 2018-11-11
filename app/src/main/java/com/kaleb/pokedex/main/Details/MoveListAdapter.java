91ffbb7e07ad88a8068c2f9242c171746b733486
Billy Kaleb <billykaleb@yahoo.com>
2018-11-12 05:15:34 +0900

    [Function] - List View Adapter untuk Move List

---
+++ app/src/main/java/com/kaleb/pokedex/main/Details/MoveListAdapter.java
@@ -0,0 +1,13 @@
+package com.kaleb.pokedex.main.Details;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MoveListAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move_list_adapter);
+    }
+}
