391ebaf2f1d39874f13d60f03e5b9409a2b8136e
Billy Kaleb <billykaleb@yahoo.com>
2018-11-11 19:29:33 +0900

    [Function] - Handling Edittext input, both using enter key and button presses

--- app/src/main/java/com/kaleb/pokedex/main/PokemonViewActivity.java
+++ app/src/main/java/com/kaleb/pokedex/main/PokemonViewActivity.java
@@ -2,6 +2,7 @@ package com.kaleb.pokedex.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -35,6 +36,27 @@ public class PokemonViewActivity extends AppCompatActivity implements PokemonVie
         imageView = findViewById(R.id.imageViewPokemon);
         textView = findViewById(R.id.pokemonName);
 
+        editText.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (event != null) {
+                    // if shift key is down, then we want to insert the '\n' char in the TextView;
+                    // otherwise, the default action is to send the message.
+                    if (!event.isShiftPressed()) {
+                        String inputResult = editText.getText().toString();
+                        closeKeyboard();
+                        showToast(inputResult);
+                        return true;
+                    }
+                    return false;
+                }
+                String inputResult = editText.getText().toString();
+                closeKeyboard();
+                showToast(inputResult);
+                return true;
+            }
+        });
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,6 +65,7 @@ public class PokemonViewActivity extends AppCompatActivity implements PokemonVie
                 showToast(inputResult);
             }
         });
+
     }
 
     @Override
@@ -60,12 +83,13 @@ public class PokemonViewActivity extends AppCompatActivity implements PokemonVie
         Toast.makeText(this, toast, Toast.LENGTH_SHORT).show();
     }
 
-    public void closeKeyboard(){
+    public void closeKeyboard() {
         try {
-            InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
         } catch (Exception e) {
 
         }
     }
+
 }
